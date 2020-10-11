0ecac2e5d637fb6609f6a3e639a698911fda25c4
Maria <48991614+marytsuk@users.noreply.github.com>
2020-10-11 18:00:27 +0300

    Add files via upload

---
+++ 01/01.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <stdio.h>
+using namespace std;
+class Allocator
+{
+	char* start = nullptr;
+	char* end = nullptr;
+	char* offset = nullptr;
+public:
+	void makeAllocator(size_t maxsize);
+	char* alloc(size_t size);
+	void reset();
+	~Allocator();
+};
+void Allocator::makeAllocator(size_t maxsize)
+{
+	if (start == nullptr)
+	{
+		start = new char[maxsize];
+		end = start + maxsize;
+		offset = start;
+	}
+	else
+	{
+		delete[] start;
+		start = new char[maxsize];
+		end = start + maxsize;
+		offset = start;
+	}
+}
+char* Allocator::alloc(size_t size)
+{
+	if (size == 0)
+	{
+		return nullptr;
+	}
+	else
+	{
+		if (offset + size <= end)
+		{
+			offset += size;
+			return offset - size;
+		}
+		else
+		{
+			return nullptr;
+		}
+	}
+}
+void Allocator::reset()
+{
+	offset = start;
+}
+Allocator::~Allocator()
+{
+	if (start != nullptr)
+	{
+		delete[] start;
+	}
+}
+int main(int argc, char** argv)
+{
+	if (argc < 2 || atoi(argv[1]) == 0)
+	{
+		cout << "No arguments!" << endl;
+		return 0;
+	}
+	Allocator test;
+	size_t maxSize = atoi(argv[1]);
+	int allocsNum = atoi(argv[2]);
+	test.makeAllocator(maxSize);
+//	test.makeAllocator(6);	
+	for (int i = 0; i < allocsNum; i++)
+	{
+		char* ptr = test.alloc(atoi(argv[3 + i]));
+		if (ptr == nullptr)
+		{
+			cout << "Out of size!" << endl << "Wanted to alloc + "<< atoi(argv[3 + i]) << ", step: " << i + 1 << endl;
+			return 0;
+		}
+	}
+	return 0;
+}
